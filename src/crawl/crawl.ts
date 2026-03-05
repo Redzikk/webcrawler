ec43f57d4a270c51a2189f68649eceae1c2a1aa5
Redzikk <mateuszredzik@Mateuszs-MacBook-Air-2.local>
2026-03-05 20:10:41 +0100

    add fetchHTML function

--- src/crawl/crawl.ts
+++ src/crawl/crawl.ts
@@ -12,3 +12,25 @@ export const normalizeUrl = (url: string) => {
     : urlObject.pathname;
   return `${urlObject.hostname}${formattedPathname}`;
 };
+
+export const fetchHTML = async (url: string) => {
+  try {
+    const res = await fetch(url, {
+      headers: {
+        // add User-Agent to avoid being banned by servers :)
+      },
+    });
+    if (res.status >= 400) {
+      console.error("something went wrong");
+      return;
+    }
+    if (res.headers.get("content-type") !== "text/html") {
+      console.error("response content type isn't html");
+      return;
+    }
+    console.log(await res.text());
+  } catch (err) {
+    console.log("ERROR: ", err);
+    return;
+  }
+};
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,15 @@
-console.log("Hello, World!");
+import { fetchHTML } from "./crawl/crawl";
+import extractPageData from "./extractPageContent/extractPageContent";
+
+const main = async () => {
+  const url = process.argv[2];
+  if (!url || process.argv.length > 3) {
+    console.error("We need exact 1 URL");
+    process.exit(1);
+  }
+  console.log("===START CRAWLING===");
+  await fetchHTML(url);
+  process.exit(0);
+};
+
+main();
